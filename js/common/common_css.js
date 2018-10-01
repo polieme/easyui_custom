49bad9acd687ed10161a88e1314dfcd7e41951ba
Zhangpeng <polieme@126.com>
2018-10-02 00:05:22 +0800

    Initial commit

---
+++ js/common/common_css.js
@@ -0,0 +1,2 @@
+document.write('<link rel="stylesheet" type="text/css" href="../../plugins/easyui/jquery-easyui-1.6.4/themes/metro/easyui.css">')
+document.write('<link rel="stylesheet" type="text/css" href="../../plugins/easyui/jquery-easyui-1.6.4/themes/icon.css">')
---
+++ js/common/common_js.js
@@ -0,0 +1,3 @@
+document.write('<script type="text/javascript" src="../../plugins/easyui/jquery-easyui-1.6.4/jquery.min.js"></script>');
+document.write('<script type="text/javascript" src="../../plugins/easyui/jquery-easyui-1.6.4/jquery.easyui.min.js"></script>');
+document.write('<script type="text/javascript" src="../../plugins/easyui/jquery-easyui-1.6.4/locale/easyui-lang-zh_CN.js"></script>');
