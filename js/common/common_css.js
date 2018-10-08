f9df5b03dabd5587285fd0a62f8dfc8430004277
Daniel <polieme@126.com>
2018-10-08 19:11:35 +0800

    1、添加字体图标、表单、选项卡、树

--- js/common/common_css.js
+++ js/common/common_css.js
@@ -1,2 +1,3 @@
 document.write('<link rel="stylesheet" type="text/css" href="../../plugins/easyui/jquery-easyui-1.6.4/themes/metro/easyui.css">')
 document.write('<link rel="stylesheet" type="text/css" href="../../plugins/easyui/jquery-easyui-1.6.4/themes/icon.css">')
+document.write('<link rel="stylesheet" type="text/css" href="../../css/common/common_css.css">')
--- js/common/common_js.js
+++ js/common/common_js.js
@@ -1,3 +1,4 @@
 document.write('<script type="text/javascript" src="../../plugins/easyui/jquery-easyui-1.6.4/jquery.min.js"></script>');
 document.write('<script type="text/javascript" src="../../plugins/easyui/jquery-easyui-1.6.4/jquery.easyui.min.js"></script>');
 document.write('<script type="text/javascript" src="../../plugins/easyui/jquery-easyui-1.6.4/locale/easyui-lang-zh_CN.js"></script>');
+document.write('<script type="text/javascript" src="../../font/font_icon_font/iconfont.js"></script>');
